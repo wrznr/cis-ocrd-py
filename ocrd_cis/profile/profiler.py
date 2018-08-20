74ab3d76898c6d31ac22413666b0a646f1047792
flo <flo@cis.lmu.de>
2018-08-20 16:53:22 +0200

    Profile is written to json output file

--- ocrd_cis/profile/profiler.py
+++ ocrd_cis/profile/profiler.py
@@ -1,13 +1,9 @@
 import json
-import re
 from ocrd import Processor
-from ocrd import MIMETYPE_PAGE
 from ocrd_cis import get_ocrd_tool
 from ocrd.utils import getLogger
 from ocrd.model.ocrd_page import from_file
 from ocrd_cis import JavaProcess
-from ocrd.model.ocrd_page_generateds import TextEquivType
-from ocrd.model.ocrd_page import to_xml
 
 
 class Profiler(Processor):
@@ -20,52 +16,11 @@ class Profiler(Processor):
 
     def process(self):
         profile = self.read_profile()
-        files = self.add_suggestions(profile)
-        for (pcgts, ifile) in files:
-            self.add_output_file(
-                ID="{}_{}".format(ifile.ID, self.output_file_grp),
-                mimetype=MIMETYPE_PAGE,
-                content=to_xml(pcgts),
-                file_grp=self.output_file_grp,
-                basename=ifile.basename,
-            )
-        self.workspace.save_mets()
-
-    def add_suggestions(self, profile):
-        files = []
-        ids = set()
-        for (word, pcgts, ifile) in self.get_all_words():
-            self.add_candidates(profile, word)
-            if ifile.ID not in ids:
-                ids.add(ifile.ID)
-                files.append((pcgts, ifile))
-        return files
-
-    def add_candidates(self, profile, word):
-        _unicode = word.get_TextEquiv()[0].Unicode
-        clean = re.sub(r'^\W*(.*?)\W*$', r'\1', _unicode)
-        lower = clean.lower()
-        if lower not in profile['data']:
-            return
-        for cand in profile['data'][lower]['Candidates']:
-            eq = TextEquivType(
-                dataType='profiler-candidate',
-                dataTypeDetails=json.dumps(cand),
-                Unicode=Profiler.format_candidate(clean, cand['Suggestion']),
-                conf=cand['Weight'],
-            )
-            word.add_TextEquiv(eq)
-            self.log.debug("suggestion: [%s] %s (%f)",
-                           clean, eq.Unicode, cand['Weight'])
-
-    def format_candidate(origin, cand):
-        res = ""
-        for (i, c) in enumerate(cand):
-            if i < len(origin) and origin[i].isupper():
-                res += c.upper()
-            else:
-                res += c
-        return res
+        self.add_output_file(
+            mimetype="application/json",
+            content=json.dumps(profile),
+            basename="profile.json",
+        )
 
     def read_profile(self):
         _input = []
@@ -79,7 +34,7 @@ class Profiler(Processor):
                  self.parameter['profilerLanguage'],
              ]
         )
-        return p.run("\n".join(_input))
+        return json.loads(p.run("\n".join(_input)))
 
     def get_all_lines(self):
         """Returns a list of tuples of lines, their parent and
