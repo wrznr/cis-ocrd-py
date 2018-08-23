58520af7cdcb903e4322c495d69e6ca3a1cd1eb6
flo <flo@cis.lmu.de>
2018-08-23 15:32:02 +0200

    Fix problems with xml file paths

--- ws-tool/ocrd-wstool.py
+++ ws-tool/ocrd-wstool.py
@@ -15,14 +15,20 @@ def unpack(fromdir, todir):
 
     path, dirs, files = os.walk(fromdir).__next__()
     for file in files:
-        filedir = os.path.join(fromdir, file)
-        with ZipFile(filedir, 'r') as myzip:
+        zipfile = os.path.join(fromdir, file)
+        print("unzipping: {} to {}".format(zipfile, todir))
+        with ZipFile(zipfile, 'r') as myzip:
             myzip.extractall(todir)
 
 
 def subprocess_cmd(command):
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True) #, executable='/bin/bash')
+    print("calling command {}".format(command.replace("\\s+", " ")))
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     out, err = process.communicate(command.encode('utf-8'))
+    ret = process.wait()
+    if ret != 0 or err is not None:
+        raise Exception("error calling command: {} (returned {})"
+                        .format(err, ret))
     print(out.decode('utf-8'))
 
 
@@ -45,7 +51,7 @@ def main(argv):
     # unpack zip files into temp file
     unpack(gtdir, tempdir)
 
-    actualfolder = os.getcwd()
+    # actualfolder = os.getcwd()
 
     os.chdir(wsdir)
 
@@ -59,54 +65,51 @@ def main(argv):
     path, dirs, files = os.walk(tempdir).__next__()
     for d in dirs:
         filedir = os.path.join(tempdir, d, d)
-        path2, dirs2, tiffiles = os.walk(filedir).__next__()
+        for path2, dirs2, tiffiles in os.walk(filedir):
+            for tif in tiffiles:
+                if tif[-4:] == '.tif':
+                    filename = tif[:-4]
 
-        for tif in tiffiles:
-            if tif[-4:] == '.tif':
-                filename = tif[:-4]
+                    tifdir = os.path.join(filedir, tif)
+                    xmldir = os.path.join(filedir, 'page', filename + '.xml')
 
-                tifdir = os.path.join(filedir, tif)
-                xmldir = os.path.join(filedir, 'page', filename + '.xml')
+                    # add tif image to workspace
+                    filegrp = 'OCR-D-IMG'
+                    mimetype = 'image/tif'
+                    fileid = filegrp + '-' + filename
+                    grpid = fileid
+                    imgcmd = '''ocrd workspace add \
+                    --file-grp {filegrp} \
+                    --file-id {fileid} \
+                    --group-id {grpid} \
+                    --mimetype {mimetype} \
+                    {fdir}'''.format(filegrp=filegrp, fileid=fileid, grpid=grpid,
+                                     mimetype=mimetype, fdir=tifdir)
+                    subprocess_cmd(imgcmd)
 
-                # add tif image to workspace
-                filegrp = 'OCR-D-IMG'
-                mimetype = 'image/tif'
-                fileid = filegrp + '-' + filename
-                grpid = fileid
-                imgcmd = '''ocrd workspace add \
-                --file-grp {filegrp} \
-                --file-id {fileid} \
-                --group-id {grpid} \
-                --mimetype {mimetype} \
-                {fdir}'''.format(filegrp=filegrp, fileid=fileid, grpid=grpid, mimetype=mimetype, fdir=tifdir)
+                    # add xml to workspace
+                    filegrp = 'OCR-D-XML'
+                    mimetype = 'application/vnd.prima.page+xml'
+                    fileid = filegrp + '-' + filename
+                    grpid = fileid
+                    xmlcmd = '''ocrd workspace add \
+                    --file-grp {filegrp} \
+                    --file-id {fileid} \
+                    --group-id {grpid} \
+                    --mimetype {mimetype} \
+                    {fdir}'''.format(filegrp=filegrp, fileid=fileid, grpid=grpid,
+                                     mimetype=mimetype, fdir=xmldir)
+                    subprocess_cmd(xmlcmd)
 
-                subprocess_cmd(imgcmd)
+                    # rename filepaths in xml into file-urls
+                    print("tif:{}, path2:{}, dirs2:{}".format(tif, path2, dirs2))
+                    sedcmd = '''
+                    sed -i {fname} -e 's#imageFilename="{tif}"#imageFilename="{fdir}"#'
+                    '''.format(fname=xmldir, tif=tif,
+                               fdir=fileprefix+wsdir+'OCR-D-IMG/'+tif)
 
-
-                #add xml to workspace
-                filegrp = 'OCR-D-XML'
-                mimetype = 'application/vnd.prima.page+xml'
-                fileid = filegrp + '-' + filename
-                grpid = fileid
-                xmlcmd = '''ocrd workspace add \
-                --file-grp {filegrp} \
-                --file-id {fileid} \
-                --group-id {grpid} \
-                --mimetype {mimetype} \
-                {fdir}'''.format(filegrp=filegrp, fileid=fileid, grpid=grpid, mimetype=mimetype, fdir=xmldir)
-                subprocess_cmd(xmlcmd)
-
-
-                # rename filepaths in xml into file-urls
-                sedcmd = '''
-                sed -i {fname}.xml -e 's#imageFilename="{tif}"#imageFilename="{fdir}"#'
-                '''.format(fname=wsdir+'OCR-D-XML/'+filename, tif=tif, fdir=fileprefix+wsdir+'OCR-D-IMG/'+tif)
-                subprocess_cmd(sedcmd)
-
+                    subprocess_cmd(sedcmd)
     shutil.rmtree(tempdir)
-
-
-
 
 
 if __name__ == '__main__':
