d1b5846e5acd9402bd9061c664ef7fd3245ba654
flo <flo@cis.lmu.de>
2018-08-24 13:32:36 +0200

    Add docstring

--- ocrd_cis/ocrd_tool.py
+++ ocrd_cis/ocrd_tool.py
@@ -3,5 +3,6 @@ from pkg_resources import resource_string
 
 
 def get_ocrd_tool():
+    """Load the json-formatted config file."""
     return json.loads(
         resource_string(__name__, 'ocrd-tool.json').decode('utf8'))
